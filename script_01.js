c651adced5fb89ea63c2215ab27f226ba190f63f
Eva <e.moritz.comcave@gmail.com>
2023-05-10 10:22:15 +0200

    if erstens

--- script_01.js
+++ script_01.js
@@ -3,3 +3,33 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
+const ageJohn = 35;  //Konstanten müssen direkt deklariert werden
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+//Wahrheitswerte sind Ergebnisse von Tests
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+//if(isJohnOlder)
+if (ageJohn > ageMark)
+{
+    console.log("John ist älter.");
+}
+
