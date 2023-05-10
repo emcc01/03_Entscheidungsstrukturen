fa219ed47925b7875418d5c6ff9da70d53c271f7
Eva <e.moritz.comcave@gmail.com>
2023-05-10 12:09:28 +0200

    switch case

--- script_01.js
+++ script_01.js
@@ -9,16 +9,16 @@ const ageMark = 30;
 // Deklaration
 let isJohnOlder, isJohnEqual;
 
-// Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// // Test | Logische Aussage
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
-// Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 //Wahrheitswerte sind Ergebnisse von Tests
 
@@ -52,15 +52,55 @@ console.log("------------------");
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
 
-if (isJohnOlder) // 1.Test
+// if (isJohnOlder) // 1.Test
+// {
+//   console.log("John ist älter.");  
+// } 
+// else if (isJohnEqual)  // 2.Test, erste Alternative
+// {
+//    console.log("John ist gleich alt."); 
+// }
+// else // 3. Test, wenn alle fehlschlagen
+// {
+//    console.log("John ist jünger."); 
+// }
+
+
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+
+const firstName = "Jane";
+let job;
+
+job = "driver";     // .. fährt TAXI! / UBER
+job = "diver";
+job = "artist";
+job = "pilot";
+job = "teacher";
+job = "instructor";
+
+switch (job) 
 {
-  console.log("John ist älter.");  
-} 
-else if (isJohnEqual)  // 2.Test, erste Alternative
-{
-   console.log("John ist gleich alt."); 
+    case "driver":  // Test auf Gleichheit
+        console.log(firstName + " fährt Taxi!");
+        break;
+    case "diver":  
+        console.log(firstName + " taucht im Rhein");
+        break;
+    case "artist":  
+        console.log(firstName + " ist Künstlerin");
+        break;   
+    case "teacher":  // Oder Verknüpfung, beide gleicher Befehl
+    case "instructor":  
+        console.log(firstName + " unterrichtet");
+        break;   
+    default:    //bei case am besten immer etwas in default schreiben
+        console.log(firstName + " macht etwas anderes");
+        break;
 }
-else // 3. Test, wenn alle fehlschlagen
-{
-   console.log("John ist jünger."); 
-}
+
+// ohne breaks würde Befehl einfach durchlaufen
+
+
+
